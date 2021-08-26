5af8db0ad15aedc97b5ac3db56e80f3b4d05f934
Dmitry Tikhonov <ddtihonov@yandex.ru>
2021-08-26 20:20:00 +0300

    popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,58 @@
 
-const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup-form');
+const popupRecord = document.querySelector('.popup-record');
+const key = "Escape";
 
 
-// отурытие всех popup
+// отурытие  popup
 function openPopup (element) {
     element.classList.add('popup_opened');
+    document.addEventListener('keydown', closeByEsc);
+    element.addEventListener('click', closeOverlay);
 }
 
-document.querySelector('.profile__changes-button').addEventListener('click', outputUser);
-document.querySelector('.profile__add-button').addEventListener('click', outpuCell);
+function closeByEsc(evt) {
+    if (evt.key === key) {
+        const closedPopup = document.querySelector('.popup_opened');
+        closePopup(closedPopup); 
+    }
+} 
 
-//закрытие всех popup
-function closePopup(evt) {
-    evt.target.closest('.popup').classList.remove('popup_opened');
+function closeOverlay (evt){
+    if (evt.target.classList.contains('popup')) {
+        const closedPopup = document.querySelector('.popup_opened');
+        closePopup(closedPopup); 
+    }
 }
+
+//закрытие popup
+function closePopup(element) {
+    element.classList.remove('popup_opened');
+    document.removeEventListener('keydown', closeByEsc);
+    element.removeEventListener('click', closeOverlay);
+}
+
+//слушатель открытия формы
+document.querySelectorAll('.button').forEach (element => {
+    element.addEventListener('click', () => {
+        openPopup(popupForm)
+    });
+});
+
+//слушатель подтверждения отправки
+document.querySelectorAll('.button-submit').forEach (element => {
+    element.addEventListener('submit', submitForm);
+});
+
 
 document.querySelectorAll('.popup__close-icon').forEach (button => {
-    button.addEventListener('click', closePopup);
-})
+    button.addEventListener('click', function (evt){
+        const elementClose = evt.target.closest('.popup');
+        closePopup(elementClose);
+});
+});
 
+function submitForm (evt) {
+    evt.preventDefault();
+    console.log('приветыыыыыыыыыыыыыыыы')
+}
